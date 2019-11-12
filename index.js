d837d6af02f7346e729823992f185dec0e298ecd
ramiromateo <30836413+ramiromateo@users.noreply.github.com>
2019-11-11 23:13:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -20,75 +20,27 @@ app.get('/', (req, res) => {
 
 app.get('/desactivar', (req, res) => {
     console.log("Desactivando");
-  var doc1 = {'tipo':'desactivar'};
-  /*MongoClient.connect("mongodb+srv://Lizama:Micronpe.-1@proyectoarqui-ydupz.gcp.mongodb.net/test?retryWrites=true", { useNewUrlParser: true },function(err, db) {
-        if(!err) {
-            console.log("We are connected");         
-            var dbo = db.db("arqui");
-            dbo.collection("culebra").insertOne(doc1, function(err, res) {
-              if (!err){               
-                console.log("1 document inserted");
-                db.close();
-              }else console.log("Error");
-            });
-
-
-          }
-        });*/
    arreglo.push('desactivar');
   res.status(200).send({ "message":"Modo Celular desactivado"});
+	arreglo=new Array();
 });
 
 
 app.get('/pop', (req, res) => {
-    res.status(200).send({ "message":arreglo.pop()});
-  /*MongoClient.connect("mongodb+srv://Lizama:Micronpe.-1@proyectoarqui-ydupz.gcp.mongodb.net/test?retryWrites=true", { useNewUrlParser: true },function(err, db) {
-        if(!err) {
-            console.log("We are connected");         
-            var dbo = db.db("arqui");
-            dbo.collection("culebra").findOne({}, function(err, result) {
-    			if (err) throw err;
-	    		if(result){
-	    			console.log(result.tipo);
-	    			res.status(200).send({ "message":result.tipo});
-	    			var myquery = { 'tipo': result.tipo };
-	  				dbo.collection("culebra").deleteOne(myquery, function(err, obj) {
-	    				if (err) throw err;
-	    				console.log("1 document deleted");
-	    				db.close();
-	  				});
-	  			}else{
-	  				res.status(200).send({ "message":"none"});
-	  				db.close();
-	  			}
-  			});  			
-
-          }
-        });  */
+	console.log(arreglo.length);
+	if(arreglo.length>0)  
+		res.status(200).send({ "message":arreglo.pop()});
+	else res.status(200).send({ "message":"none"});
+  
 });
 
 app.get('/texto', function(req, res){
-    console.log('POST /');
+    console.log('Palabra almacenada /');
     console.log(req.query.palabra);
     var doc1 = {'tipo':req.query.palabra};
     arreglo.push(req.query.palabra);
 
-  /*	MongoClient.connect("mongodb+srv://Lizama:Micronpe.-1@proyectoarqui-ydupz.gcp.mongodb.net/test?retryWrites=true", { useNewUrlParser: true },function(err, db) {
-        if(!err) {
-            console.log("We are connected");         
-            var dbo = db.db("arqui");
-            dbo.collection("culebra").insertOne(doc1, function(err, res) {
-              if (!err){               
-                console.log("1 document inserted");
-                db.close();
-              }else{
-                console.log("Error");
-              }
-            });
-
-
-          }
-        });  */
+  
     res.status(200).send({ "message":"Palabra Registrada"});
 });
 
